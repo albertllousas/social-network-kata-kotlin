a18fbec5538f0a1a44865783da83fc6881c1047e
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-20 16:41:32 +0100

    Refactor SocialNetwork: refactor format function

--- src/main/kotlin/kata/SocialNetwork.kt
+++ src/main/kotlin/kata/SocialNetwork.kt
@@ -7,8 +7,8 @@ import java.time.LocalDateTime.now
 class SocialNetwork(
     private val posts: Posts,
     private val followers: Followers,
-    private val printLine: (String) -> Unit ,
-    private val clock: Clock
+    private val printLine: (String) -> Unit,
+    private val clock: Clock,
 ) {
 
     fun submitCommand(command: String) = when {
@@ -28,13 +28,16 @@ class SocialNetwork(
     private fun viewWall(userName: String) {
         val users = followers.findFollowedBy(userName) + userName
         val posts = users.flatMap { user -> posts.findBy(user).map { Pair(user, it) } }
-        return posts.sortedByDescending { it.second.timestamp }.forEach { printLine("${it.first} - ${it.second.format()}") }
+        return posts.sortedByDescending { it.second.timestamp }.forEach { printLine(it.second.format(it.first)) }
     }
 
-    private fun Post.format(): String {
-        val minutesAgo = Duration.between(now(clock), timestamp).toMinutes()
-        return if (minutesAgo < 1L) return "$message (${Duration.between(now(clock), timestamp).toSeconds()} seconds ago)"
-        else if (minutesAgo == 1L) return "$message (1 minute ago)"
-        else "$message ($minutesAgo minutes ago)"
-    }
+    private fun Post.format(userName: String? = null) =
+        Pair(Duration.between(now(clock), timestamp), userName?.let { "$userName - " } ?: "")
+            .let { (elapsedTime, user) ->
+                when {
+                    elapsedTime.toMinutes() < 1L -> "$user$message (${elapsedTime.seconds} seconds ago)"
+                    elapsedTime.toMinutes() == 1L -> "$user$message (1 minute ago)"
+                    else -> "$user$message (${elapsedTime.toMinutes()} minutes ago)"
+                }
+            }
 }
