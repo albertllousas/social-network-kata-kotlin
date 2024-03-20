382e9731a2367359654de078a9545b3c59a39406
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-03-20 23:46:54 +0100

    Refactor PostFormatter: imperativize too functional code

--- src/main/kotlin/kata/PostFormatter.kt
+++ src/main/kotlin/kata/PostFormatter.kt
@@ -5,13 +5,13 @@ import java.time.Duration
 import java.time.LocalDateTime
 
 object PostFormatter {
-    fun format(post: Post, clock: Clock, showUser: Boolean): String =
-        Pair(Duration.between(LocalDateTime.now(clock), post.timestamp), if (showUser) "${post.username} - " else "")
-            .let { (elapsedTime, user) ->
-                when {
-                    elapsedTime.toMinutes() < 1L -> "$user${post.message} (${elapsedTime.seconds} seconds ago)"
-                    elapsedTime.toMinutes() == 1L -> "$user${post.message} (1 minute ago)"
-                    else -> "$user${post.message} (${elapsedTime.toMinutes()} minutes ago)"
-                }
-            }
+    fun format(post: Post, clock: Clock, showUser: Boolean): String {
+        val elapsedTime = Duration.between(LocalDateTime.now(clock), post.timestamp)
+        val user = if (showUser) "${post.username} - " else ""
+        return when {
+            elapsedTime.toMinutes() < 1L -> "$user${post.message} (${elapsedTime.seconds} seconds ago)"
+            elapsedTime.toMinutes() == 1L -> "$user${post.message} (1 minute ago)"
+            else -> "$user${post.message} (${elapsedTime.toMinutes()} minutes ago)"
+        }
+    }
 }
